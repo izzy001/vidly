ed2a1cdc23b89788766af0bd7a032bbb3318dd20
israel emoitologa <israelemoitologa@israels-MacBook-Pro.local>
2021-09-24 22:11:14 +0100

    fixed joi  module require error

--- routes/genres.js
+++ routes/genres.js
@@ -3,7 +3,7 @@ const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const { Genre, validate } = require('../models/genre');
 const mongoose = require('mongoose');
-const express = require('express');
+const express = require('express');   
 const router = express.Router();
 
 
